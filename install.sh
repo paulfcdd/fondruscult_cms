475a57e9a6871543c430d0434c2881796f8615ab
Paul <paulfcdd@gmail.com>
2017-11-08 15:51:58 +0100

    small fix in install sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 echo '
 ****************************************
 *                                      *
@@ -11,11 +12,12 @@ php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" &&
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" &&
 php composer-setup.php --install-dir=bin &&
 php -r "unlink('composer-setup.php');"
-echo '2.INSTALLING DEPENDENCIES'
-php bin/composer.phar install
+
+echo '2.INSTALL PHP & JS DEPENDENCIES'
+php bin/composer.phar install && cd web/ && npm install
 
 echo '3. CREATING DATABASE'
-php bin/console doctrine:database:create
+cd ../ && php bin/console doctrine:database:create
 
 echo "4. IMPORT DATABASE SCHEME"
 php bin/console doctrine:schema:update --force
@@ -29,7 +31,7 @@ php bin/console cache:clear --no-warmup && php bin/console cache:clear --no-warm
 echo '
 ****************************************
 *                                      *
-* Installation process complete        *
+*    Installation process complete     *
 *                                      *
 ****************************************
 '
