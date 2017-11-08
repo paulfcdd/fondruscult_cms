0786ee9761b2aba1d6fe7ce2379c2a8c5e7ea0a6
Paul <paulfcdd@gmail.com>
2017-11-08 15:58:54 +0100

    changes in update sh file

--- update.sh
+++ update.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/env bash
 echo '
 ****************************************
 *                                      *
-*       Starting system updating       *
+*       Starting system update         *
 *                                      *
 ****************************************
 '
@@ -9,17 +10,28 @@ echo '
 echo '1. CHECK REPOSITORY'
 git pull
 
+echo '2.INSTALL PHP & JS DEPENDENCIES'
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" &&
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" &&
+php composer-setup.php --install-dir=bin &&
+php -r "unlink('composer-setup.php');" &&
+php bin/composer.phar install &&
+cd web/ &&
+npm install
+
 echo '2. UPDATING DATABASE'
-bin/console doctrine:schema:update --force
+cd ../ && bin/console doctrine:schema:update --force
 
 echo '3. CLEARING THE CACHE'
-bin/console cache:clear --no-warmup
-bin/console cache:clear --no-warmup  --env=prod
+bin/console cache:clear --no-warmup &&
+bin/console cache:clear --no-warmup --env=prod &&
+php -r "unlink('bin/composer.phar');"
+
 
 echo '
 ****************************************
 *                                      *
-*     System updating finished!        *
+*      System update finished!         *
 *                                      *
 ****************************************
 '
